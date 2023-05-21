97630308668ba0e237c3dca4e2d96050eb9bc4ce
bertin <bertin.m2001@gmail.com>
2023-05-21 17:09:34 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./work/*.html'],
+  theme: {
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976',
+      xl:'144px'
+    },
+    extend: {
+      colors:{
+        brightRed:'hsl(12,88%,59%)',
+        brightRedLight:'hsl(12,88%,69%)',
+        brightRedSupLight:'hsl(12,88%,59%)',
+        darkBlue:'hsl(228,39%,23%)',
+        darkGrayishBlue:'hsl(227,12%,61%)',
+        varyDarkBlue:'hsl(233,12%,13%)',
+        veryPaleRed:'hsl(13,100%,96%)',
+        veryLightGray:'hsl(0,0%,98%)'
+
+      }
+    },
+  },
+  plugins: [],
+}
+
